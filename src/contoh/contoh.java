dfe0b002d99a088030db255043b290f1b6a3523e
Lukman Kurniawan <campione47@gmail.com>
2017-06-15 20:15:39 +0700

    Git Menggunakan NetBeans

---
+++ src/contoh/contoh.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contoh;
+
+import contoh.view.FrmHello;
+
+/**
+ *
+ * @author USER
+ */
+public class contoh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmHello h = new FrmHello();
+        h.setVisible(true);
+        
+    }
+    
+}
